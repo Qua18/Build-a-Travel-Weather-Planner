c7288a296d6811c59b4a88d5be25c5a58c2d7748
Qua18 <nnquaok22@gmail.com>
2026-09-11 17:18:57 +0700

    Add if

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ is_raining = True
 has_bike = True
 has_car = True
 has_ride_share_app = True
+if distance_mi:
+    print('False')
+if distance_mi <= 1 and is_raining != True:
+    print('True')
+elif distance_mi <= 6 and has_bike == True and is_raining != True :
+        print('True')
+elif distance_mi > 6 and (has_car == True or has_ride_share_app == True) :
+    print('True')
+else:
+    print('False')
+
+
+
+
